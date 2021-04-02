31670a0cb0bcd9d5af6457b7b816367feeec0cd9
sant720 <santiago.bolivar7@gmail.com>
2021-04-02 17:56:01 -0500

    Add search feature

--- project-1/wiki/encyclopedia/urls.py
+++ project-1/wiki/encyclopedia/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/", views.wiki, name="wiki"),
-    path("wiki/<str:entry>", views.entry, name="entry")
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("search/", views.search, name="search")
 ]
--- project-1/wiki/encyclopedia/views.py
+++ project-1/wiki/encyclopedia/views.py
@@ -18,3 +18,18 @@ def entry(request, entry):
         "body": util.markdowner(entry)
     })
 
+def search(request):
+    if request.method == "POST":
+        q = request.POST['q'].strip()
+        matches = []
+        for entry in util.list_entries():
+            q_low, entry_low = q.lower(), entry.lower()
+            if q_low == entry_low:
+                return HttpResponseRedirect("/wiki/" + q)
+            if q_low in entry_low:
+                matches.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "q": q,
+            "matches": matches
+        })
+    return HttpResponseRedirect("/")
